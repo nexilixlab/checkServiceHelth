cafa29b3e1a80d0a895b0331c1ba17c6a03e0078
git <eae966@gmail.com>
2023-03-26 16:34:15 +0430

    Some Updates.

--- checkService.go
+++ checkService.go
@@ -1,95 +1,211 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"encoding/xml"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"strconv"
-	"strings"
 	"time"
-
-	"github.com/nexilixlab/checkServiceHelth/config"
 )
 
-func checkBlock() (int, error) {
-	resp, err := http.Get("http://127.0.0.1:8545")
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-
-	var result map[string]interface{}
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		return 0, err
-	}
-
-	blockNumberHex := result["result"].(string)
-	blockNumber, err := strconv.ParseInt(blockNumberHex[2:], 16, 64)
-	if err != nil {
-		return 0, err
-	}
-
-	return int(blockNumber), nil
+type Service struct {
+	Name string `json:"name"`
 }
 
-func restartService(service string) error {
-	cmd := exec.Command("systemctl", "restart", service)
-	if err := cmd.Run(); err != nil {
+type Config struct {
+	Jsonrpc string    `json:"jsonrpc"`
+	Id      int       `json:"id"`
+	Method  string    `json:"method"`
+	Params  []Service `json:"params"`
+}
+
+func readConfig(filename string) (Config, error) {
+	var config Config
+
+	configFile, err := os.Open(filename)
+	if err != nil {
+		return config, err
+	}
+
+	defer configFile.Close()
+
+	jsonParser := json.NewDecoder(configFile)
+	if err = jsonParser.Decode(&config); err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
+
+func writeData(block string) error {
+	xmlString := []byte("<?xml version=\"1.0\" encoding=\"UTF-8\"?><config><lastblock>" + block + "</lastblock></config>")
+	err := ioutil.WriteFile("config.xml", xmlString, 0644)
+	if err != nil {
 		return err
 	}
 
 	return nil
 }
 
+func restartService(serviceName string, logger *log.Logger) error {
+	logger.Printf("Trying to restart service %s\n", serviceName)
+
+	cmd := exec.Command("systemctl", "restart", serviceName)
+	err := cmd.Run()
+
+	if err != nil {
+		logger.Printf("Failed to restart service %s: %s\n", serviceName, err.Error())
+		return err
+	}
+
+	logger.Printf("Successfully restarted service %s\n", serviceName)
+
+	return nil
+}
+
 func main() {
+	configFilePath := flag.String("configFilePath", "", "Path to the config file")
+	flag.Parse()
+
+	configFile := "config.json"
+
+	if *configFilePath != "" {
+		configFile = *configFilePath
+	}
+
+	config, err := readConfig(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	logFile, err := os.OpenFile("/var/log/checkNexilix/service.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer logFile.Close()
 
 	logger := log.New(logFile, "", log.LstdFlags)
-	servicesPtr := flag.String("services", "", "comma-separated list of services to restart")
-	flag.Parse()
 
-	services := strings.Split(*servicesPtr, ",")
-
-	if len(services) == 0 {
-		//fmt.Println("Error: at least one service name should be provided")
-		logger.Printf("Error: at least one service name should be provided")
-		return
-	}
 	for {
-		conf, err := config.Read()
+		time.Sleep(5 * time.Minute)
+
+		method := config.Method
+		params := config.Params
+
+		url := fmt.Sprintf("%s", config.Jsonrpc)
+		payload, err := json.Marshal(map[string]interface{}{
+			"jsonrpc": "2.0",
+			"id":      1,
+			"method":  method,
+			"params":  params,
+		})
 		if err != nil {
-			//fmt.Println("Error: ", err)
-			logger.Printf("Error: %s", err)
-			return
+			logger.Printf("Failed to create payload: %s\n", err.Error())
+			continue
 		}
 
-		blockNumber, err := checkBlock()
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		req, err := http.NewRequestWithContext(ctx, "POST", url, nil)
 		if err != nil {
-			logger.Printf("Error: %s", err)
-			return
+			logger.Printf("Failed to create HTTP request: %s\n", err.Error())
+			continue
 		}
 
-		if blockNumber != conf.Block {
-			if err := config.Write(blockNumber); err != nil {
-				logger.Printf("Error: %s", err)
-				return
-			}
+		req.Header.Set("Content-Type", "application/json")
+		req.Body = ioutil.NopCloser((string(payload)))
 
-			for _, service := range services {
-				if err := restartService(service); err != nil {
-					logger.Printf("Error: %s", err)
-					return
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			logger.Printf("Failed to send HTTP request: %s\n", err.Error())
+		}
+
+		defer resp.Body.Close()
+
+		respBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			logger.Printf("Failed to read response body: %s\n", err.Error())
+			continue
+		}
+
+		var respData map[string]interface{}
+
+		if err := json.Unmarshal(respBody, &respData); err != nil {
+			logger.Printf("Failed to parse response body: %s\n", err.Error())
+			continue
+		}
+
+		result, ok := respData["result"].(map[string]interface{})
+		if !ok {
+			logger.Println("Failed to parse result field in response")
+			continue
+		}
+
+		block, ok := result["number"].(string)
+		if !ok {
+			logger.Println("Failed to parse block number from response")
+			continue
+		}
+
+		lastBlock, err := strconv.Atoi(string(block))
+		if err != nil {
+			logger.Printf("Failed to parse block number as integer: %s\n", err.Error())
+			continue
+		}
+
+		lastBlockString := strconv.Itoa(lastBlock)
+
+		xmlFile, err := os.Open("config.xml")
+		if err != nil {
+			logger.Printf("Failed to open config.xml: %s\n", err.Error())
+			continue
+		}
+
+		defer xmlFile.Close()
+
+		xmlData, err := ioutil.ReadAll(xmlFile)
+		if err != nil {
+			logger.Printf("Failed to read config.xml: %s\n", err.Error())
+			continue
+		}
+
+		var configData map[string]string
+
+		if err := xml.Unmarshal(xmlData, &configData); err != nil {
+			logger.Printf("Failed to parse config.xml: %s\n", err.Error())
+			continue
+		}
+
+		previousBlock, ok := configData["lastblock"]
+		if !ok {
+			logger.Println("Failed to find last block number in config.xml")
+			continue
+		}
+
+		if previousBlock == lastBlockString {
+			logger.Printf("Server is healthy. Last block is %s and current block is %s\n", previousBlock, lastBlockString)
+		} else {
+			logger.Printf("Trying to restart services...\n")
+
+			for _, service := range config.Params {
+				if err := restartService(service.Name, logger); err != nil {
+					logger.Printf("Failed to restart service %s: %s\n", service.Name, err.Error())
 				}
 			}
 		}
 
-		time.Sleep(5 * time.Minute)
+		if err := writeData(lastBlockString); err != nil {
+			logger.Printf("Failed to write block number to config.xml: %s\n", err.Error())
+		}
 	}
 }
