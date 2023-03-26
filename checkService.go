765ddae76e66c4621dd350933a1451a5ee4d938d
git <eae966@gmail.com>
2023-03-26 16:26:09 +0430

    Some Updates.

--- checkService.go
+++ checkService.go
@@ -3,8 +3,9 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
@@ -44,37 +45,46 @@ func restartService(service string) error {
 }
 
 func main() {
+	logFile, err := os.OpenFile("/var/log/checkNexilix/service.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+
+	logger := log.New(logFile, "", log.LstdFlags)
 	servicesPtr := flag.String("services", "", "comma-separated list of services to restart")
 	flag.Parse()
 
 	services := strings.Split(*servicesPtr, ",")
 
 	if len(services) == 0 {
-		fmt.Println("Error: at least one service name should be provided")
+		//fmt.Println("Error: at least one service name should be provided")
+		logger.Printf("Error: at least one service name should be provided")
 		return
 	}
 	for {
 		conf, err := config.Read()
 		if err != nil {
-			fmt.Println("Error: ", err)
+			//fmt.Println("Error: ", err)
+			logger.Printf("Error: %s", err)
 			return
 		}
 
 		blockNumber, err := checkBlock()
 		if err != nil {
-			fmt.Println("Error: ", err)
+			logger.Printf("Error: %s", err)
 			return
 		}
 
 		if blockNumber != conf.Block {
 			if err := config.Write(blockNumber); err != nil {
-				fmt.Println("Error: ", err)
+				logger.Printf("Error: %s", err)
 				return
 			}
 
 			for _, service := range services {
 				if err := restartService(service); err != nil {
-					fmt.Println("Error: ", err)
+					logger.Printf("Error: %s", err)
 					return
 				}
 			}
